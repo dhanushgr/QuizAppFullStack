cf5c70b0319434d1d8d6b3ae0c3b7de8912e8150
dhanushgr <grdhanush8@gmail.com>
2025-01-31 06:21:09 +0530

    Created Project QuizAppFullStack connected postgres to the project (resources/application.properties) added dependencies to the pom.xml file created README.md file

---
+++ src/main/java/com/dhanush/QuizAppFullStack/QuizAppFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.dhanush.QuizAppFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhanush/QuizAppFullStack/QuizAppFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanush.QuizAppFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
