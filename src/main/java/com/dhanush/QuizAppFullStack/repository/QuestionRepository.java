7b4f67b5558bd28edf003f8921212f1d63cd1439
dhanushgr <grdhanush8@gmail.com>
2025-01-31 14:50:15 +0530

    added custom methods to the repository which was required

--- src/main/java/com/dhanush/QuizAppFullStack/repository/QuestionRepository.java
+++ src/main/java/com/dhanush/QuizAppFullStack/repository/QuestionRepository.java
@@ -1,8 +1,18 @@
 package com.dhanush.QuizAppFullStack.repository;
 
 import com.dhanush.QuizAppFullStack.model.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.net.ContentHandler;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    @Query("SELECT DISTINCT q.subject FROM Question q")
+    List<String> findDistinctSubject(); //customized method
+
+    Page<Question> findBySubject(String subject, Pageable pageable);
 }
