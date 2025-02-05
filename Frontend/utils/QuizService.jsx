f3f9c6e950159c22ef5769f7130471ce63d491fb
dhanushgr <grdhanush8@gmail.com>
2025-02-05 11:58:04 +0530

    changed the code in QuizService.jsx

--- Frontend/utils/QuizService.jsx
+++ Frontend/utils/QuizService.jsx
@@ -1,71 +1,72 @@
+import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:9192/api/quiz"
+	baseURL: "http://localhost:9192/api/quiz"
 })
 
-export const createQuestion = async(quizQuestion) =>{
-    try {
-        const response = await api.post("/create-new-question", quizQuestion)
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
+export const createQuestion = async(quizQustion) =>{
+  try {
+    const response = await api.post("/create-new-question", quizQustion)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const getAllQuestions = async() =>{
-    try {
-        const response = await api.get("/all-questions")
-        return response.data
-    } catch (error) {
-        console.error(error)
-        return []
-    }
+  try {
+    const response = await api.get("/all-questions")    
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
 
-export const getQuestionsForUser = async(number, subject) =>{
-    try {
-        const response = await api.get(`/fetch-questions-for-user?numOfQuestion=${number}&subject=${subject}`)
-        return response.data
-    } catch (error) {
-        console.error(error)
-        return []
-    }
+export const fetchQuizForUser = async(number, subject) =>{
+  try {
+    const response = await api.get(
+			`/quiz/fetch-questions-for-user?numOfQuestions=${number}&subject=${subject}`
+		)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
 
-export const getAllSubjects = async() =>{
-    try {
-        const response = await api.get("/subjects")
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
+export const getSubjects = async() =>{
+  try {
+    const response = await api.get("/subjects")
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-export const updateQuestions = async(id, question) =>{
-    try {
-        const response = await api.put(`/question/update/${id}`, question)
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
+export const updateQuestion = async(id, question) =>{
+  try {
+    const response = await api.put(`/question/${id}/update`, question)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const getQuestionById = async(id) =>{
-    try {
-        const response = await api.get(`/question/${id}`)
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await api.get(`/question/${id}`)
+		return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const deleteQuestion = async(id) =>{
-    try {
-        const response = await api.delete(`question/delete/${id}`)
-        return response.data
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await api.delete(`/question/${id}/delete`)
+		return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
-
-
