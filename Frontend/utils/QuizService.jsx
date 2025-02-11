ed6bd2079f8c8488891074d7f915402064ea909c
dhanushgr <grdhanush8@gmail.com>
2025-02-11 06:11:54 +0530

    changed the base url to dynamically get the url

--- Frontend/utils/QuizService.jsx
+++ Frontend/utils/QuizService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:9192/api/quiz"
+	baseURL: `${import.meta.env.VITE_API_URL}/api/quiz`
 })
 
 export const createQuestion = async (quizQuestion) => {
