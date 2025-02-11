1b6fb686d46e0ba8cae86d89f4d7ade81c05defc
dhanushgr <grdhanush8@gmail.com>
2025-02-11 06:14:33 +0530

    changed the url, password, username to get the info dynamically from .env file created a varaible which takes url form application.properties

--- Backend/src/main/java/com/dhanush/QuizAppFullStack/config/CorsConfig.java
+++ Backend/src/main/java/com/dhanush/QuizAppFullStack/config/CorsConfig.java
@@ -1,12 +1,17 @@
 package com.dhanush.QuizAppFullStack.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.SqlReturnType;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173") // Allow frontend URL
+                        .allowedOrigins(frontendUrl) // Allow frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
